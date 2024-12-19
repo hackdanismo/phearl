49ae97da5a5ab4dd42f8ea4d6e9e6d84f2079561
hackdanismo <office@justtoconfirm.co.uk>
2024-12-19 13:53:51 +0000

    feat: move the database credentials to const variables inside of a config.php file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// Database configuration
+
+/*
+const over define:
+- Works with classes, namespaces, and global scope.
+- Resolved at compile-time, making it slightly faster.
+- Cleaner syntax, preferred for modern PHP projects. Define compatible with PHP 5.3 or earlier.
+*/
+
+const DB_HOSTNAME = "localhost";
+const DB_USERNAME = "username";
+const DB_PASSWORD = "password";
+const DB_DATABASE = "phearl_db";
--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
     // File will open in MAMP PRO: https://pherl.local:8890/
 
-    // Database connection variables
-    $hostname = "localhost";
-    $username = "username";
-    $password = "password";
-    $database = "phearl_db";
+    // Include the config file for the database credentials
+    require_once "config.php";
 
     try {
         // Create a PDO instance with MySQL DSN (Data Source Name)
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn = new PDO("mysql:host=" . DB_HOSTNAME . ";dbname=" . DB_DATABASE, DB_USERNAME, DB_PASSWORD);
         // Set the PDO error mode to exception for better error handling
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
-        // TODO: remove echo statement and replace with loggin mechanism
+        // TODO: remove echo statement and replace with logging mechanism
         echo "Connection to the database failed: " . $e->getMessage();
     }
