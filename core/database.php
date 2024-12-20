ed8a00b1b3a1ac9cb440074fb8f76aa0021c7c5d
hackdanismo <office@justtoconfirm.co.uk>
2024-12-20 13:58:56 +0000

    feat: update the Database class with revised and new code comments with the introduction of a destructor method to cleanup and close the connection to the database when the script ends execution or the object is destroyed

--- core/database.php
+++ core/database.php
@@ -1,5 +1,6 @@
 <?php
 class Database {
+    // Private properties to store the database credentials and the connection itself
     private $host;
     private $dbName;
     private $username;
@@ -8,7 +9,7 @@ class Database {
 
     // Constructor initializes and creates the connection to the database
     public function __construct($host = null, $dbName = null, $username = null, $password = null) {
-        // Include the configuration file with the database credentials
+        // Include the configuration file with the default database credentials
         require_once __DIR__ . "/../config/config.php";
 
         // Use provided parameters or fallback to constants from the configuration file
@@ -16,11 +17,15 @@ class Database {
         $this->dbName = $dbName ?? DB_DATABASE;
         $this->username = $username ?? DB_USERNAME;
         $this->password = $password ?? DB_PASSWORD;
-
+        
+        // Initialize the connection property as null, as no active connection yet
         $this->conn = null;
 
+        // Attempt to establish a connection to the database using the credentials provided
         try {
+            // Create a new PDO (PHP Data Objects) instance with the connection details
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->username, $this->password);
+            // Set the PDO error mode to Exception to allow error handling through exceptions
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             // TODO: Implement an improved logging mechanism to replace the use of echo
@@ -28,8 +33,15 @@ class Database {
         }
     }
 
-    // Getter function for the connection
+    // Public method to retrieve the active database connection without exposing the credentials
     public function getConnection() {
+        // Return the PDO connection object
         return $this->conn;
+    }
+
+    // Destructor method to clean up resources when the object is destroyed or when the script execution ends
+    public function __destruct() {
+        // Explicitly set the connection to null to close the database connection
+        $this->conn = null;
     }
 }
